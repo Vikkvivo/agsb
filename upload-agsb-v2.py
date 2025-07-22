42048f18ae78dfdad8cb6717ab6cff833cb9fff5
Vikas rawat <85556962+Vikkvivo@users.noreply.github.com>
2025-07-22 23:58:22 +0530

    upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytdlala"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
+USER_NAME = "vikkvivo"         # 用户名
+UUID = "490f5418-c8c4-4688-b6a3-4d6bd79196c3"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "idx.vikas7898.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiYjE0Mjg2NDY0ODQyMzQ4YzQ1ZDA0NmQ2NzI1NzAwMGIiLCJ0IjoiMjEwOTI2MjItOTM3Yy00MzAxLWJjZDgtZWEzNjlmMTVkNTIzIiwicyI6InVvRzVTVGpZcjRxT1JldUVSaXlRQmxLbGx5T0pheXVNdWRTTjVtdXVCSFE9In0="                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
