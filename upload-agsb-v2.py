b9208378339beeabaf6b2e78770b05be5c89e4c2
Vikas rawat <85556962+Vikkvivo@users.noreply.github.com>
2025-07-23 00:14:02 +0530

    upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -33,7 +33,7 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 # ====== 全局可配置参数（可直接在此处修改） ======
 USER_NAME = "vikkvivo"         # 用户名
 UUID = "490f5418-c8c4-4688-b6a3-4d6bd79196c3"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
+PORT = 25511                   # Vmess端口，留空或0则自动生成
 DOMAIN = "idx.vikas7898.dpdns.org"                   # 域名，留空则自动获取
 CF_TOKEN = "eyJhIjoiYjE0Mjg2NDY0ODQyMzQ4YzQ1ZDA0NmQ2NzI1NzAwMGIiLCJ0IjoiMjEwOTI2MjItOTM3Yy00MzAxLWJjZDgtZWEzNjlmMTVkNTIzIiwicyI6InVvRzVTVGpZcjRxT1JldUVSaXlRQmxLbGx5T0pheXVNdWRTTjVtdXVCSFE9In0="                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
